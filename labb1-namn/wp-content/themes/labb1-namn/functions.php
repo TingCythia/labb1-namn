ea05e0cb5cbc947b91abbb942170b419aa4535aa
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-04-05 20:08:50 +0200

    add css,font,image,js and functions.php

---
+++ labb1-namn/wp-content/themes/labb1-namn/functions.php
@@ -0,0 +1,20 @@
+<?php 
+/** 
+*@package labb1-namn
+*/
+
+add_theme_support('post-thumnails');
+add_theme_support('menus');
+/**
+ * Set up content width
+ */
+
+if ( ! isset( $content_width ) )
+ $content_width = 800; /* pixels */
+
+/**
+ * check if theme is existing, the function will hook the themes
+ */
+
+
+?>
